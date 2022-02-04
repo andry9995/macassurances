d6d41238a454fb7d097ee5dd3da1772dc36d4f0d
andry9995 <andriramananarivo@gmail.com>
2022-02-04 09:40:48 +0300

    cron process

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -8,146 +8,84 @@ class Cron extends MY_Controller {
 		parent::__construct();
 		$this->load->library('shissab');
 		$this->load->model('AproposModel');
-		$this->load->model('SectionModel');
-		$this->load->model('CustomerModel');
-		$this->load->model('SliderModel');
-		$this->load->model('ActualiteModel');
-		$this->load->model('ProduitModel');
-		$this->load->model('BureauModel');
-		$this->load->model('MembreModel');
 	}
 
 	public function run()
 	{
 
-		$this->apropos();
-		$this->sections();
-		$this->customers();
-		$this->sliders();
-		$this->actualites();
-		$this->produits();
-		$this->bureaux();
-		$this->membres();
+		$entities = array(
+			array(
+				'api' => 'apropos',
+				'model' => 'AproposModel',
+				'multiple' => false
+			),
+			array(
+				'api' => 'sections',
+				'model' => 'SectionModel',
+				'multiple' => true
+			),
+			array(
+				'api' => 'customers',
+				'model' => 'CustomerModel',
+				'multiple' => true
+			),
+			array(
+				'api' => 'sliders',
+				'model' => 'SliderModel',
+				'multiple' => true
+			),
+			array(
+				'api' => 'actualites',
+				'model' => 'ActualiteModel',
+				'multiple' => true
+			),
+			array(
+				'api' => 'produits',
+				'model' => 'ProduitModel',
+				'multiple' => true
+			),
+			array(
+				'api' => 'bureaux',
+				'model' => 'BureauModel',
+				'multiple' => true
+			),
+			array(
+				'api' => 'membres',
+				'model' => 'MembreModel',
+				'multiple' => true
+			)
+		);
+
+		$this->process($entities);
 	}
 
-	public function membres()
+	public function process($entities)
 	{
+		foreach ($entities as $entity) {
 
-        $membres = (array)$this->shissab->membres();
+			$api = $entity['api'];
+			$model = $entity['model'];
+			$multiple = $entity['multiple'];
 
-        foreach ($membres as $membre) {
+			$this->load->model($model);
 
-        	$membre = (array)$membre;
+			$data = (array)$this->shissab->$api();
 
-        	$this->fetch( 
-        		$membre, 
-        		$this->MembreModel 
-        	);
-        }
+			if (!!$multiple) {
+		        foreach ($data as $item) {
 
-	}
+		        	$item = (array)$item;
 
-	public function bureaux()
-	{
-
-        $bureaux = (array)$this->shissab->bureaux();
-
-        foreach ($bureaux as $bureau) {
-
-        	$bureau = (array)$bureau;
-
-        	$this->fetch( 
-        		$bureau, 
-        		$this->BureauModel 
-        	);
-        }
-
-	}
-
-	public function produits()
-	{
-
-        $produits = (array)$this->shissab->produits();
-
-        foreach ($produits as $produit) {
-
-        	$produit = (array)$produit;
-
-        	$this->fetch( 
-        		$produit, 
-        		$this->ProduitModel 
-        	);
-        }
-
-	}
-
-	public function actualites()
-	{
-
-        $actualites = (array)$this->shissab->actualites();
-
-        foreach ($actualites as $actualite) {
-
-        	$actualite = (array)$actualite;
-
-        	$this->fetch( 
-        		$actualite, 
-        		$this->ActualiteModel 
-        	);
-        }
-
-	}
-
-	public function sliders()
-	{
-
-        $sliders = (array)$this->shissab->sliders();
-
-        foreach ($sliders as $slider) {
-
-        	$slider = (array)$slider;
-
-        	$this->fetch( 
-        		$slider, 
-        		$this->SliderModel 
-        	);
-        }
-
-	}
-
-	public function customers()
-	{
-
-        $customers = (array)$this->shissab->customers();
-
-        foreach ($customers as $customer) {
-
-        	$customer = (array)$customer;
-
-        	$this->fetch( 
-        		$customer, 
-        		$this->CustomerModel 
-        	);
-        }
-
-	}
-
-
-	public function sections()
-	{
-
-        $sections = (array)$this->shissab->sections();
-
-        foreach ($sections as $section) {
-
-        	$section = (array)$section;
-
-        	$this->fetch( 
-        		$section, 
-        		$this->SectionModel 
-        	);
-        }
-
+		        	$this->fetch( 
+		        		$item, 
+		        		$this->$model 
+		        	);
+		        }
+			} else {
+        		$this->fetch($data, $this->$model);
+			}
+			
+		}
 	}
 
 	public function apropos()
