45798f7f4c60297aef02a70a83b1ca88a559c18a
andry9995 <andriramananarivo@gmail.com>
2022-02-07 19:13:24 +0300

    member slide

--- application/views/contact/home-customers.php
+++ application/views/contact/home-customers.php
@@ -7,7 +7,6 @@
     <div class="container">
         <div class="row">
             <div class="col">
-
                 <div class="slider responsive">
                     <?php foreach ($customers as $customer): ?>
                         <div class="mac_slider_item">
@@ -16,22 +15,7 @@
                             </div>
                         </div>
                     <?php endforeach ?>
-
                 </div>
-
-                <!-- <div class="mac_slider_container p-50">
-                    <div class="owl-carousel owl-theme customer_slider">
-                        <?php foreach ($customers as $customer): ?>
-                            <div class="owl-item">
-                                <div class="mac_slider_item customer_item">
-                                    <div class="mac_slider_img">
-                                        <img src="<?php echo $customer->img ?>" alt="">
-                                    </div>
-                                </div>
-                            </div>
-                        <?php endforeach ?>
-                    </div> 
-                </div> -->
             </div>
         </div>
     </div>
--- application/views/histoire/index.php
+++ application/views/histoire/index.php
@@ -15,7 +15,7 @@
     <div class="product-device product-device-2 box-shadow d-none d-md-block"></div>
 </div>
 
-<section class="section bg-lightgray">
+<section class="section">
 	<header class="container text-center">
         <h1 class="section-title"><?php echo $apropos->titre ?></h1>
     </header>
@@ -30,43 +30,30 @@
     </div>
 </section>
 
-<section id="aboutus" class="section bg-lightgray">
+<section id="aboutus" class="section">
     <header class="container text-center">
-        <h1 class="section-title text-left">Notre Équipe</h1>
+        <h1 class="section-title text-center">Notre Équipe</h1>
     </header>
 
     <div class="container">
-        <div class="row">
-            <?php foreach ($membres as $membre): ?>
-            	<div class="col-sm-6 col-lg-2 member-container">
-	                <div class="member left">
-	                    <div class="member-wrapper">
-	                        <figure class="front">
-	                            <img src="<?php echo $membre->img ?>" alt="<?php echo $membre->nom ?>" class="img-responsive">
-	                        </figure>
-	                        <div class="back">
-	                            <div class="vcenter-container">
-	                                <div class="vcenter">
-	                                    <h2><?php echo $membre->nom ?></h2>
-	                                    <p><?php echo $membre->poste ?></p>
-	                                    <hr>
-	                                    <!-- <div class="socials socials-small">
-	                                        <a href="#" class="social facebook" data-bs-placement="top" data-bs-toggle="tooltip" data-bs-custom-class="social-facebook" title="Follow me on Facebook"><i class="fab fa-facebook-f"></i></a>
-	                                        <a href="#" class="social twitter" data-bs-placement="top" data-bs-toggle="tooltip" data-bs-custom-class="social-twitter" title="Follow me on Twitter"><i class="fab fa-twitter"></i></a>
-	                                        <a href="#" class="social instagram" data-bs-placement="top" data-bs-toggle="tooltip" data-bs-custom-class="social-instagram" title="Follow me on Instagram"><i class="fab fa-instagram"></i></a>
-	                                        <a href="#" class="social linkedin" data-bs-placement="top" data-bs-toggle="tooltip" data-bs-custom-class="social-linkedin" title="Find me on Linkedin"><i class="fab fa-linkedin"></i></a>
-	                                    </div> -->
-	                                </div><!-- End .vcenter -->
-	                            </div><!-- End .vcenter-container -->
-
-	                        </div><!-- End .back -->
-	                    </div><!-- End .member-wrapper -->
-	                </div><!-- End .member -->
-	            </div><!-- End .col-lg-3 -->
-            <?php endforeach ?>
-        </div><!-- End .row -->
-    </div><!-- End .container -->
-
+    	<div class="row">
+    		<div class="col">
+    			<div class="slider responsive">
+                    <?php foreach ($membres as $membre): ?>
+                        <div class="mac_slider_item">
+                            <div class="mac_slider_img ">
+                                <img src="<?php echo $membre->img ?>" alt="">
+                                <div class="member-desc text-center">
+                                    <h2><?php echo $membre->nom ?></h2>
+                                    <h3><?php echo $membre->poste ?></h3>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach ?>
+                </div>
+    		</div>
+    	</div>
+    </div>
     <div class="md-margin d-md-none"></div><!-- space -->
     
 </section>
